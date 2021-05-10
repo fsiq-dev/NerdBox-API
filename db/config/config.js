262f67ca37f6fb39a0d5519bc07dd25812c6e998
Felipe <felipe.msiqueira@al.infnet.edu.br>
2021-05-09 22:58:44 -0300

    config postgres

--- db/config/config.js
+++ db/config/config.js
@@ -1,47 +1,18 @@
-const node_environment = process.env.NODE_ENV || "development";
-
-if (node_environment === "development") {
-  require("dotenv").config();
-}
-
-// module.exports = {
-//   database: process.env.DB_DATABASE,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   dialect: process.env.DB_DIALECT,
-//   logging: false
-// };
-
+require('dotenv').config();
 module.exports = {
-  local: {
-    username: "root",
-    password: null,
-    database: "database_dev",
-    host: "127.0.0.1",
-    dialect: "postgres",
+
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
   },
-  development: {
-    username: "root",
-    password: null,
-    database: "database_dev",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-  production: {
-    uri: process.env.DATABASE_URL,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOSTL,
-    dialect: "postgres",
-  },
-};
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  }
+}
