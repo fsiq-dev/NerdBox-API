b75088bab5e9dbf153834a15059489907175ede0
Felipe <felipe.msiqueira@al.infnet.edu.br>
2021-05-10 07:09:13 -0300

    fixconfig.js configdb

--- db/config/config.js
+++ db/config/config.js
@@ -1,20 +1,36 @@
-require('dotenv').config();
+const node_environment = process.env.NODE_ENV || 'development'
+
+if (node_environment === 'development') {
+
+  require('dotenv').config();
+
+}
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres"
-  },
-  "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": { rejectUnauthorized: false }
-    }
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  dialect: "postgres",
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false }
   }
 }
+// module.exports = {
+//   "development": {
+//     "username": process.env.DB_USER,
+//     "password": process.env.DB_PASS,
+//     "dialect": "postgres",
+//     "database": process.env.DB_DATABASE,
+//     "host": process.env.DB_HOST,
+//   },
+//   "production": {
+//     "username": process.env.DB_USER,
+//     "password": process.env.DB_PASS,
+//     "dialect": "postgres",
+//     "database": process.env.DB_DATABASE,
+//     "host": process.env.DB_HOST,
+//     "dialectOptions": {
+//       "ssl": { rejectUnauthorized: false }
+//     }
+//   }
+// }
